0d9a54b89cc6e42f3341df1cca4a485325b2cda1
Andy Wu <www.flamer1959@hotmail.com>
2023-02-04 22:38:57 -0800

    Fixes for Collisions

--- Assets/LetterManager.cs
+++ Assets/LetterManager.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LetterManager : MonoBehaviour
-{   
-    private GameObject[] BadColliderList;
-    public ColliderScript[] ColliderList;
+{  
+    private List<ColliderScript> GigaBadColliderList;
+    public List<ColliderScript> ColliderList;
+    public List<ColliderScript> BadCollidersList;
+    public List<ColliderScript> MiniorColliders;
+    public Transform ParentCollider;
+    public Transform ParentBadCollider;
+    public Transform ParentMinorCollider;
+    public Transform ParentGigaBadCollidser;
     private float NumberOfHitColliders = 0;
     private float Score = 0;
     private bool IsLetterComplete = false;
@@ -13,25 +19,32 @@ public class LetterManager : MonoBehaviour
     void Start()
     {
         NumberOfHitColliders = 0;
-        int index = 0;
-        ColliderList = new ColliderScript[transform.childCount];
-        foreach (Transform collider in transform) {
-            ColliderList[index] = (collider.GetComponent<ColliderScript>());
-            index = index + 1;
-        }
+        foreach(Transform child in ParentCollider) {
+
+            ColliderList.Add(child.GetComponent<ColliderScript>());
+        };
+        foreach(Transform child in ParentGigaBadCollidser) {
+            GigaBadColliderList.Add(child.GetComponent<ColliderScript>());
+        };
+        foreach(Transform child in ParentBadCollider) {
+            BadCollidersList.Add(child.GetComponent<ColliderScript>());
+        };
+        foreach(Transform child in ParentMinorCollider) {
+            MiniorColliders.Add(child.GetComponent<ColliderScript>());
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-        Score = NumberOfHitColliders / ColliderList.Length;
+        Score = NumberOfHitColliders / ColliderList.Count;
         NumberOfHitColliders = 0;   
         foreach (ColliderScript Collider in ColliderList) {
             if(Collider.HasCollided()) {
                 NumberOfHitColliders = NumberOfHitColliders + 1;
             }
         }
-        if(NumberOfHitColliders == ColliderList.Length) { 
+        if(NumberOfHitColliders == ColliderList.Count) { 
             //Insert Whatever
             Debug.Log("YOU WIN!");
         }
