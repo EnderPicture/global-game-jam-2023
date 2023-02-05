c7a589d4a41b6f77186cfa61235307ac3e53b8b8
Alvin <ho.alvin857@gmail.com>
2023-02-05 12:27:41 -0800

    Start screen

---
+++ Assets/start.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Animation
+            // Invoke("loadScene", 1f);
+            loadScene();
+        }
+    }
+
+    void loadScene() {
+        SceneManager.LoadScene("alvinMult", LoadSceneMode.Single);
+    }
+}
